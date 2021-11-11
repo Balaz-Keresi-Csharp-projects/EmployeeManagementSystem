ef1ec4223db322323536c51a34efc6edb3c9f3dd
balaz.keresi <kobalu@gmail.com>
2021-11-11 19:01:49 +0100

    Initializing the Project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmployeeManagementSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The aim of this project is to develop an Employee Management System.");
+            Console.WriteLine("...Work in progress");
+            Console.ReadLine();
+        }
+    }
+}
