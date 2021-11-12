a29e2b48d438c2f8955dec39896fc57729a79471
balaz.keresi <kobalu@gmail.com>
2021-11-12 11:11:20 +0100

    Realizing the User Interface

---
+++ EmployeeManagementSystem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem
+{
+    class EmployeeManagementSystem
+    {
+        private Dictionary<string, string> availableActions;
+        public EmployeeManagementSystem()
+        {
+            availableActions = new Dictionary<string, string>();
+            availableActions.Add("1", "List all of the employees in the system");
+            availableActions.Add("2", "Report some hours of work for the employee");
+            availableActions.Add("3", "Calculate the wage of an employee");
+            availableActions.Add("4", "Pay the wage for the employee");
+            availableActions.Add("5", "Relist all of the available functions");
+            availableActions.Add("Exit", "Exit");
+        }
+        public void Activate()
+        {
+            Console.WriteLine(ListAvailableActions());
+            bool exit = false;
+            while (!exit)
+            {
+                Console.Write("Chose your action: ");
+                string action = Console.ReadLine();
+                switch (action)
+                {
+                    case "Exit":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Sorry, the instruction {0} is not defined", action);
+                        break;
+                }
+            }
+        }
+
+        public String ListAvailableActions()
+        {
+            StringBuilder outputString = new StringBuilder();
+            outputString.AppendLine("The Employee Management System allows you to do the next operations:");
+            foreach(var action in availableActions)
+            {
+                Console.WriteLine("\t{0}. {1}", action.Key, action.Value);
+            }
+
+            return outputString.ToString();
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,9 +6,19 @@ namespace EmployeeManagementSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("The aim of this project is to develop an Employee Management System.");
-            Console.WriteLine("...Work in progress");
-            Console.ReadLine();
+            Console.WriteLine("**********************************************************************************************************************");
+            Console.WriteLine("*** The aim of this software is to realise a command line Employee Management System.                              ***");
+            Console.WriteLine("*** This project is ment for practicing C# object oriented features and may not sufficient for professional usage. ***");
+            Console.WriteLine("*** The idea of this porject comes from the Pluralsight course: Introduction to the C# Type System by Gill Cleeren ***");
+            Console.WriteLine("*** Check out the course for yourself:                                                                             ***");
+            Console.WriteLine("*** \thttps://app.pluralsight.com/library/courses/introduction-c-sharp-type-sysyem/table-of-contents               ***");
+            Console.WriteLine("**********************************************************************************************************************");
+            Console.WriteLine();
+
+            EmployeeManagementSystem employeeManagementSystem = new EmployeeManagementSystem();
+
+            employeeManagementSystem.Activate();
+            
         }
     }
 }
