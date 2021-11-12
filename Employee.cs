a3db7bd541ca04020b3984a3377026c971e1b1c3
balaz.keresi <kobalu@gmail.com>
2021-11-12 12:16:24 +0100

    Defining Employees and list them.

---
+++ Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem
+{
+    class Employee
+    {
+
+        public Employee(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+        public string firstName { get; }
+        public string lastName { get; }
+        public string Name
+        {
+            get { return String.Format("{0} {1}", firstName, lastName); }
+        }
+    }
+}
--- EmployeeManagementSystem.cs
+++ EmployeeManagementSystem.cs
@@ -8,6 +8,9 @@ namespace EmployeeManagementSystem
 {
     class EmployeeManagementSystem
     {
+        // Idea: It is posible to refactor the availableActions dictinoary to create a class. That way the logic to select and parse the actions can be removed to EmployeeManagementSystem
+        List<Employee> employees = new List<Employee>();
+
         private Dictionary<string, string> availableActions;
         public EmployeeManagementSystem()
         {
@@ -16,8 +19,13 @@ namespace EmployeeManagementSystem
             availableActions.Add("2", "Report some hours of work for the employee");
             availableActions.Add("3", "Calculate the wage of an employee");
             availableActions.Add("4", "Pay the wage for the employee");
-            availableActions.Add("5", "Relist all of the available functions");
-            availableActions.Add("Exit", "Exit");
+            availableActions.Add("5", "List all of the available actions");
+            availableActions.Add("Q", "Exit");
+
+            employees.Add(new Employee("Jovan", "Andric"));
+            employees.Add(new Employee("Milica", "Komosar"));
+            employees.Add(new Employee("Aneta", "Derkovic"));
+            employees.Add(new Employee("John", "Smith"));
         }
         public void Activate()
         {
@@ -29,7 +37,13 @@ namespace EmployeeManagementSystem
                 string action = Console.ReadLine();
                 switch (action)
                 {
-                    case "Exit":
+                    case "1":
+                        Console.WriteLine(ListAllEmployees());
+                        break;
+                    case "5":
+                        Console.WriteLine(ListAvailableActions());
+                        break;
+                    case "Q":
                         exit = true;
                         break;
                     default:
@@ -45,7 +59,20 @@ namespace EmployeeManagementSystem
             outputString.AppendLine("The Employee Management System allows you to do the next operations:");
             foreach(var action in availableActions)
             {
-                Console.WriteLine("\t{0}. {1}", action.Key, action.Value);
+                outputString.AppendLine(String.Format("\t{0}. {1}", action.Key, action.Value));
+            }
+
+            return outputString.ToString();
+        }
+
+        public String ListAllEmployees()
+        {
+            StringBuilder outputString = new StringBuilder();
+            outputString.AppendLine();
+            outputString.AppendLine("The Employees managed by the System:");
+            foreach (var employee in employees)
+            {
+                outputString.AppendLine(String.Format("\t{0}", employee.Name));
             }
 
             return outputString.ToString();
